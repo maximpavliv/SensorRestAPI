4c68dd2e4574574530694edb82161a1816d6074f
maximpavliv <maxim.pavliv@gmail.com>
2024-03-26 16:31:40 +0100

    Init

---
+++ src/main/java/ua/max/springcourse/SensorRestAPI/SensorRestApiApplication.java
@@ -0,0 +1,13 @@
+package ua.max.springcourse.SensorRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/max/springcourse/SensorRestAPI/SensorRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.max.springcourse.SensorRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
